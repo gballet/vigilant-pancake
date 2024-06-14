56a7500c326c73196b94145abce0920077ab3642
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-06-14 14:50:28 +0200

    add module

--- build.zig
+++ build.zig
@@ -36,6 +36,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    _ = b.addModule("testdep", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
